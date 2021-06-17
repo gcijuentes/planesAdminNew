825959f4763dcb836d52e985c2eb09991aaf3fa2
Gabriel cifuentes <gcifuent@everis.com>
2021-06-17 10:40:31 -0400

    second version

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,41 +1,39 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
 import { LoginComponent } from './auth/login/login.component';
 import { RegisterComponent } from './auth/register/register.component';
-import { DashboardComponent } from './pages/dashboard/dashboard.component';
-import { GraficaComponent } from './pages/grafica/grafica.component';
 import { NopagefoundComponent } from './pages/nopagefound/nopagefound.component';
-import { ProgressComponent } from './pages/progress/progress.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
 import { BreadcrumbsComponent } from './shared/breadcrumbs/breadcrumbs.component';
-import { HeaderComponent } from './shared/header/header.component';
-import { AppRoutingModule } from './app-routing.module';
 import { SidebarComponent } from './shared/sidebar/sidebar.component';
-import { NuevoComponent } from './pages/nuevo/nuevo.component';
-import { Header2Component } from './shared/header2/header2.component';
+import { HeaderComponent } from './shared/header/header.component';
+import { ProgressComponent } from './pages/progress/progress.component';
+import { Grafica1Component } from './pages/grafica1/grafica1.component';
+import { PagesComponent } from './pages/pages.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
     RegisterComponent,
-    DashboardComponent,
-    GraficaComponent,
     NopagefoundComponent,
-    ProgressComponent,
+    DashboardComponent,
     BreadcrumbsComponent,
-    HeaderComponent,
-    HeaderComponent,
     SidebarComponent,
-    NuevoComponent,
-    Header2Component
+    HeaderComponent,
+    ProgressComponent,
+    Grafica1Component,
+    PagesComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
   ],
-  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
